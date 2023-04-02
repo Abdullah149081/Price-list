eb37dc676f267b58fc726bfbe04cdb7ed6d43a19
Abdullah149081 <masudmd425@gmail.com>
2023-04-02 16:47:30 +0600

    header part design done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -34,7 +34,7 @@ const Header = () => {
   ];
 
   return (
-    <nav className="md:px-32 px-2 flex justify-between pt-5 items-center">
+    <nav className="md:px-32 bg-slate-900 px-2 flex justify-between pt-5 items-center">
       <h2 className="text-2xl md:text-3xl font-bold text-primary">Embars Membership</h2>
 
       <div className="relative ">
@@ -45,10 +45,14 @@ const Header = () => {
         >
           <span className="pr-12 md:pr-0">
             {" "}
-            {isOpen ? <Bars3Icon className="h-6 w-6 mt-7 text-blue-700 md:hidden cursor-pointer"></Bars3Icon> : <XMarkIcon className="h-6 w-6 mt-7 text-blue-700 md:hidden cursor-pointer"></XMarkIcon>}
+            {isOpen ? <Bars3Icon className="h-7 w-6 mt-7  text-secondary md:hidden cursor-pointer"></Bars3Icon> : <XMarkIcon className="h-7 w-6 mt-7 text-error md:hidden cursor-pointer"></XMarkIcon>}
           </span>
         </div>
-        <ul className={`md:flex  gap-11 duration-500  font-bold text-xl absolute md:relative md:right-0  ${isOpen ? "-right-36" : "right-6"}`}>
+        <ul
+          className={`md:flex gap-11 bg-slate-900  rounded-bl-lg duration-500 md:duration-0 p-10   font-bold text-xl absolute md:relative md:right-0 md:top-0  ${
+            isOpen ? "-right-44" : "-right-2 top-[75px]"
+          }`}
+        >
           {routesArray.map((route) => (
             <Link route={route} key={route.id}></Link>
           ))}
--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Link = ({ route }) => {
   return (
     <>
-      <a href={route.path} >
-        <li className="mt-3 md:mt-0">{route.name}</li>
+      <a href={route.path}>
+        <li className="mt-3 md:mt-0 text-primary hover:text-purple-500">{route.name}</li>
       </a>
     </>
   );
