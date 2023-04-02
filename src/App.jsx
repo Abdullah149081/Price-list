f156d9e7aba727e5d89802aa037cd133095f9017
Abdullah149081 <masudmd425@gmail.com>
2023-04-02 17:46:19 +0600

    membership components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Membership from "./components/Membership/Membership";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Membership></Membership>
     </div>
   );
 }
---
+++ src/components/Membership/Membership.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Membership = () => {
+  return (
+    <div className="px-2 md:px-32 py-14">
+      <h2 className="text-xl md:text-5xl text-center font-bold">Choose a plan that's right for you</h2>
+    </div>
+  );
+};
+
+export default Membership;
