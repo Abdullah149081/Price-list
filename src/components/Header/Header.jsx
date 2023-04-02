59f710771b17614a926be47c56f9f41f9ee4caa6
Abdullah149081 <masudmd425@gmail.com>
2023-04-02 16:07:09 +0600

    navbar toggel done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -34,18 +34,21 @@ const Header = () => {
   ];
 
   return (
-    <nav className="md:px-32 flex justify-between pt-5">
-      <h2 className="text-3xl font-bold text-primary">Embars Membership</h2>
+    <nav className="md:px-32 px-2 flex justify-between pt-5 items-center">
+      <h2 className="text-2xl md:text-3xl font-bold text-primary">Embars Membership</h2>
 
-      <div>
+      <div className="relative ">
         <div
           onClick={() => {
             setIsOpen(!isOpen);
           }}
         >
-          {isOpen ? <Bars3Icon className="h-6 w-6 text-blue-700 md:hidden cursor-pointer"></Bars3Icon> : <XMarkIcon className="h-6 w-6 text-blue-700 md:hidden cursor-pointer"></XMarkIcon>}
+          <span className="pr-12 md:pr-0">
+            {" "}
+            {isOpen ? <Bars3Icon className="h-6 w-6 mt-7 text-blue-700 md:hidden cursor-pointer"></Bars3Icon> : <XMarkIcon className="h-6 w-6 mt-7 text-blue-700 md:hidden cursor-pointer"></XMarkIcon>}
+          </span>
         </div>
-        <ul className="md:flex gap-11 font-bold text-xl">
+        <ul className={`md:flex  gap-11 duration-500  font-bold text-xl absolute md:relative md:right-0  ${isOpen ? "-right-36" : "right-6"}`}>
           {routesArray.map((route) => (
             <Link route={route} key={route.id}></Link>
           ))}
--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -1,12 +1,10 @@
-
 import React from "react";
 
 const Link = ({ route }) => {
   return (
     <>
-     
-      <a href={route.path}>
-        <li>{route.name}</li>
+      <a href={route.path} >
+        <li className="mt-3 md:mt-0">{route.name}</li>
       </a>
     </>
   );
